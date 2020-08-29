edbc9401f098db7c2506e88a9150752ff22fa936
Smit Panchal <sm414905@dal.ca>
2020-08-29 11:52:31 -0300

    Matrix Calculation added

---
+++ DataTransformer.java
@@ -0,0 +1,32 @@
+
+public class DataTransformer {
+
+	public boolean clear (  ) {
+		return false;
+	}
+
+	public Integer read( String filename ) {
+		
+		return 0;
+	
+	}
+
+	public boolean newColumn ( String columnName ) {
+		return false;
+	}
+
+	public Integer calculate( String equation ) {
+		return 0;
+	}
+
+	public void top(  ) {
+	}
+
+	public void print(  ) {
+	}
+
+	public Integer write( String filename ) {
+		return 0;
+	}
+
+}
